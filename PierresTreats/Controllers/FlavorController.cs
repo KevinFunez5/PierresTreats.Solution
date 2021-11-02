a5257439546d1d7e043d20b53e5d87835d3e90bc
Kevin Funez <kevin.funez315@gmail.com>
2021-11-01 21:58:51 -0700

    add authorization to treats controller

--- PierresTreats/Controllers/FlavorController.cs
+++ PierresTreats/Controllers/FlavorController.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using PierresTreats.Models;
 using System.Linq;
+using System;
 
 namespace PierresTreats.Controllers
 {
   public class FlavorController : Controller
   {
     private readonly PierresTreatsContext _db;
- 
+
     public FlavorController(PierresTreatsContext db)
     {
       _db = db;
@@ -68,6 +69,16 @@ namespace PierresTreats.Controllers
     {
       var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
       _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      Console.WriteLine("DeleteFlavor route");
+      var joinEntry = _db.FlavorTreats.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreats.Remove(joinEntry);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
